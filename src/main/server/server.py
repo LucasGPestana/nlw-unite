627c130c818847a9c66e2bbbf7c48046a5be84a9
Lucas Gabriel Pestana <lucasgabrielpestana03@gmail.com>
2024-04-04 16:01:15 -0300

    feat: Implementing AttendeesHandler with attendee badge route

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 
 from src.models.settings.connection import DB_CONNECTION_HANDLER
+
 from src.main.routes.event_routes import event_route_bp
+from src.main.routes.attendees_routes import attendees_route_bp
 
 DB_CONNECTION_HANDLER.connect_to_db()
 
@@ -10,3 +12,4 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(event_route_bp)
+app.register_blueprint(attendees_route_bp)
