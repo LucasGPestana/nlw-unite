95bdd7ee875a7e798823581b6af6428bd5323c6c
Lucas Gabriel Pestana <lucasgabrielpestana03@gmail.com>
2024-04-03 21:31:50 -0300

    feat: Implementing CheckInsRepository

---
+++ src/models/repository/check_ins_repository.py
@@ -0,0 +1,30 @@
+from src.models.entities.check_ins import CheckIns
+from src.models.settings.connection import DB_CONNECTION_HANDLER
+
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
+class CheckInsRepository:
+
+  def insert_check_in(self, attendee_id: str) -> str:
+
+    with DB_CONNECTION_HANDLER as database:
+
+      try:
+
+        check_in = CheckIns(
+          attendeeId=attendee_id
+        )
+
+        database.session.add(check_in)
+        database.session.commit()
+
+        return attendee_id
+      
+      except IntegrityError:
+
+        raise Exception("Check-in já cadastrado!")
+      
+      except Exception as exception:
+
+        database.session.rollback()
+        raise exception
