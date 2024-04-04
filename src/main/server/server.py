3ecc948593632a6625e1d5c4742534979c78fe93
Lucas Gabriel Pestana <lucasgabrielpestana03@gmail.com>
2024-04-04 14:52:28 -0300

    feat: Implementing get event route

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
 
+from src.models.settings.connection import DB_CONNECTION_HANDLER
 from src.main.routes.event_routes import event_route_bp
+
+DB_CONNECTION_HANDLER.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
--- src/models/repository/events_repository.py
+++ src/models/repository/events_repository.py
@@ -1,6 +1,9 @@
 from typing import Dict
 from src.models.settings.connection import DB_CONNECTION_HANDLER
+
 from src.models.entities.events import Events
+from src.models.entities.attendees import Attendees
+
 from sqlalchemy.exc import IntegrityError, NoResultFound
 
 
@@ -23,11 +26,11 @@ class EventsRepository:
                 database.session.commit()
 
                 return eventsInfo
-            
+
             except IntegrityError:
 
                 raise Exception("Evento já cadastrado!")
-            
+
             except Exception as exception:
 
                 database.session.rollback()
@@ -38,11 +41,40 @@ class EventsRepository:
         with DB_CONNECTION_HANDLER as database:
 
             try:
-            
-                event = database.session.query(Events).filter(Events.id == event_id).one()
+
+                event = database.session.query(Events).filter(
+                    Events.id == event_id).one()
 
                 return event
-            
+
             except NoResultFound:
 
                 return None
+
+    def count_event_attendees(self, event_id: str) -> Dict:
+
+        with DB_CONNECTION_HANDLER as database:
+
+
+            event_count = (
+                database.session
+                .query(Events)
+                .join(Attendees, Events.id == Attendees.event_id)
+                .filter(Events.id == event_id)
+                .with_entities(
+                    Events.maximum_attendees,
+                    Attendees.id
+                )
+                .all()
+            )
+
+            if not len(event_count):
+                return {
+                    "maximumAttendees": 0,
+                    "attendeesAmount": 0,
+                }
+                
+            return {
+                    "maximumAttendees": event_count[0].maximum_attendees,
+                    "attendeesAmount": len(event_count),
+                }
